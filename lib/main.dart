01a5426be31463976aa83114c811575c51e23ef4
Suraj Bastola <bastolasuraj@hotmail.com>
2020-07-18 20:42:31 +0545

    A Simple Quiz App created by a Flutter NOOB

--- lib/main.dart
+++ lib/main.dart
@@ -17,24 +17,67 @@ class _LearningFlutterState extends State<LearningFlutter> {
   final _questions = const [
     {
       "questionText": "What is your birth color?",
-      "answers": ["Red", "Green", "Blue", "Black", "White"]
+      "answers": [
+        {"score": 10, "text": "Red"},
+        {"score": 20, "text": "Green"},
+        {"score": 30, "text": "Blue"},
+        {"score": 40, "text": "Black"},
+        {"score": 50, "text": "White"}
+      ]
     },
     {
       "questionText": "What is your best friends name?",
-      "answers": ["John", "Oliver", "Felicity", "Thea", "Roy"]
+      "answers": [
+        {"score": 20, "text": "John"},
+        {"score": 30, "text": "Oliver"},
+        {"score": 40, "text": "Felicity"},
+        {"score": 50, "text": "Thea"},
+        {"score": 10, "text": "Roy"}
+      ]
     },
     {
       "questionText": "What is your name?",
-      "answers": ["Spartan", "Arrow", "Over Watch", "Speedy", "Arsenal"]
+      "answers": [
+        {"score": 30, "text": "Spartan"},
+        {"score": 40, "text": "Arrow"},
+        {"score": 50, "text": "Over Watch"},
+        {"score": 10, "text": "Speedy"},
+        {"score": 20, "text": "Arsenal"}
+      ]
     },
     {
       "questionText": "What is your Father's name?",
-      "answers": ["JJ", "Robert", "Gwendolyn", "Maria", "Laila"]
+      "answers": [
+        {"score": 40, "text": "JJ"},
+        {"score": 50, "text": "Robert"},
+        {"score": 10, "text": "Gwendolyn"},
+        {"score": 20, "text": "Maria"},
+        {"score": 30, "text": "Laila"}
+      ]
     },
+    {
+      "questionText": "What is your favourite animal?",
+      "answers": [
+        {"score": 50, "text": "Snake"},
+        {"score": 10, "text": "Hawk"},
+        {"score": 20, "text": "Eagle"},
+        {"score": 30, "text": "Frog"},
+        {"score": 40, "text": "Slug"}
+      ]
+    }
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestions() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestions(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -45,7 +88,7 @@ class _LearningFlutterState extends State<LearningFlutter> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("QnA"),
+          title: Text("Simple Quiz"),
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
@@ -53,7 +96,7 @@ class _LearningFlutterState extends State<LearningFlutter> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,35 @@
 import "package:flutter/material.dart";
 
 class Result extends StatelessWidget {
+  final int finalScore;
+  final Function resetQuizFunction;
+
+  Result(this.finalScore, this.resetQuizFunction);
+
+  String get resultPhrase {
+    var resultText = "You Have Completed the Quiz";
+    if (finalScore < 100) {
+      resultText = resultText + "\n You have failed the test of life";
+    } else if (finalScore <= 200) {
+      resultText = resultText + "\n You have an average life";
+    } else {
+      resultText = resultText + "\n You are a life winner";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You are DONE!!!"),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          FlatButton(onPressed: resetQuizFunction, child: Text("Reset The Quiz"))
+        ],
+      ),
     );
   }
 }
